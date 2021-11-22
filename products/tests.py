4a4367e784bab0d9622136f425d814f5831141c5
Ola Malmgren <malmgrenola@gmail.com>
2021-11-22 21:51:59 +0100

    adding products test and products sort and filter

--- products/tests.py
+++ products/tests.py
@@ -1,12 +1,39 @@
 from django.test import TestCase
 from .models import Category, Product
+from django.shortcuts import reverse
+
 
 class TestViews(TestCase):
 
     def test_get_products_index(self):
-            response = self.client.get('/products/')
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'products/products.html')
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_can_sort_products(self):
+        response = self.client.get(f'/products/?sort=price')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_can_tamp_with_url_on_sort_products(self):
+        response = self.client.get(f'/products/?sort=fish')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_can_filter_products_category(self):
+        response = self.client.get(f'/products/?c=fish')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_can_query_products(self):
+        response = self.client.get(f'/products/?q=fish')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_can_do_empty_query(self):
+        response = self.client.get(f'/products/?q=')
+        self.assertEqual(response.request['QUERY_STRING'], 'q=')
+        self.assertEqual(response.status_code, 302)
 
 
 class TestModels(TestCase):
@@ -15,7 +42,7 @@ class TestModels(TestCase):
         item = Category.objects.create(name='Test category item')
         self.assertEqual(str(item), 'Test category item')
 
-
     def test_products_string_method_returns_name(self):
-        item = Product.objects.create(name='Test product item', price=22.5, description="Item description")
+        item = Product.objects.create(
+            name='Test product item', price=22.5, description="Item description")
         self.assertEqual(str(item), 'Test product item')
--- products/views.py
+++ products/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, reverse
 from django.views.decorators.http import require_http_methods
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
+
 
 @require_http_methods(["GET"])
 def all_products(request):
@@ -10,20 +11,51 @@ def all_products(request):
 
     products = Product.objects.all()
     query = ""
+    categories = Category.objects.filter(id__in=products.values_list('id'))
+    sort = ""
+
+    if 'sort' in request.GET:
+        sort = request.GET['sort']
+        data = sort.split(":", 1)
+
+        # Parse sort data into field and sort direction
+        field = data[1] if len(data) == 2 else data[0]
+        direction = "-" if len(data) == 2 and data[0] == "desc" else ""
+
+        try:
+            products = products.order_by(f'{direction}{field}')
+        except:
+            # Triggers when user tamper with url manually... in a non suppoorted way!
+            messages.error(request, "There is an error sorting your data!")
+
+    if 'c' in request.GET:
+        categories = request.GET['c'].split(',')
+        lookup_products = Q()
+        lookup_categories = Q()
+
+        # Case insensetive filtering
+        for category in categories:
+            lookup_products |= Q(category__name__iexact=category)
+            lookup_categories |= Q(name__iexact=category)
+
+        products = products.filter(lookup_products)
+        categories = Category.objects.filter(lookup_categories)
 
     if 'q' in request.GET:
         query = request.GET['q']
         if not query:
-            messages.error(request, "To find products you must supply a query!")
+            messages.error(
+                request, "To find products you must supply a query!")
             return redirect(reverse('products'))
 
         queries = Q(name__icontains=query) | Q(description__icontains=query)
         products = products.filter(queries)
 
-
     context = {
         'products': products,
         'query': query,
+        'categories': categories,
+        'sort': sort
     }
 
     return render(request, 'products/products.html', context)
